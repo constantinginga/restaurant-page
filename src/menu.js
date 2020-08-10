fe00a0339565b889c13ef5363ce4490d0eb62582
Constantin Ginga <costelgn@gmail.com>
2020-08-10 17:57:11 +0300

    Add menu page

--- src/menu.js
+++ src/menu.js
@@ -1,33 +1,22 @@
-const menuDiv = document.createElement("div"),
-    menu = require('./menu.json');
+import {elem} from './home.js';
+const menu = require('./menu.json');
 
 
 export default function generateMenu(parent) {
-    const h1 = document.createElement("h1");
-
-    menuDiv.id = 'menu';
-    h1.innerHTML = 'Our current menu';
-    h1.id = 'title';
-    menuDiv.appendChild(h1);
-    // create menu items and their descriptions
+    const menuDiv = elem.create('div', 'menuDiv', ''),
+        h1 = elem.create('h1', 'h1', 'What we offer*'),
+        msg = [
+            elem.create('p', 'msg', menu.msg),
+            elem.create('p', 'warning', menu.warning)],
+        // stores item, description and hr
+        menuNodes = [];
+    // create each menu item
     for (let i = 0; i < menu.items.length; i++) {
-        createItem('name', i);
-        createItem('description', i);
-        menuDiv.appendChild(document.createElement("hr"));
+        menuNodes.push(
+            elem.create('p', 'item-name', menu.items[i]['name']),
+            elem.create('p', 'item-desc', menu.items[i]['description']),
+            document.createElement("hr"));
     }
-    
-    createItem('msg');
-    createItem('warning');
+    elem.append(menuDiv, h1, ...menuNodes, ...msg);
     parent.appendChild(menuDiv);
-    document.body.style.backgroundImage = 'url("../images/home.jpg")';
 }
-
-
-// create item with corresponding text and id
-function createItem(type, i) {
-    const item = document.createElement("p");
-    (arguments.length === 2) ? item.innerHTML = menu.items[i][type] : item.innerHTML = menu[type];
-    item.id = type;
-    menuDiv.appendChild(item);
-}
-
